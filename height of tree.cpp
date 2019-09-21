6b833f64aae7c63751fe8dccc2fee9a9e2dd8f74
Ved <53895870+ved-sinha@users.noreply.github.com>
2019-09-21 15:34:10 +0530

    Height of tree

---
+++ height of tree.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+    node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+    
+    if(d==-1){
+        return NULL;
+    }
+    
+    node*root=new node(d);
+    root->left=buildTree();
+    root->right=buildTree();
+    return root;
+}
+
+void printPreorder(node*root){
+    if(root==NULL)
+    return;
+    
+    cout<<root->data<<" ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+
+void printInorder(node*root){
+    if(root==NULL)
+    return;
+    
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+void printPost(node*root){
+    if(root==NULL)
+    return;
+    
+    printPost(root->left);
+    printPost(root->right);
+    cout<<root->data<<" ";
+}
+
+int height(node* root){
+    if(root==NULL)
+    return 0;
+    else return max(height(root->left),height(root->right))+1;
+}
+
+
+int main() {
+    node* root=buildTree();
+    printPreorder(root);
+	cout<<endl;
+	printInorder(root);
+	cout<<endl;
+	printPost(root);
+	cout<<endl;
+	cout<<height(root);
+	return 0;
+}
